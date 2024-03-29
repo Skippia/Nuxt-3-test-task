e5bd471c371dbad5c99ad3540db8eab39bac7e3f
Shelbymode <pocketbook.love24@gmail.com>
2024-03-29 11:25:31 +0300

    feat: add active-link functionality

--- components/user-aside/card.vue
+++ components/user-aside/card.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import type { TUser } from '~/types'
+import type { TUserFull } from '~/types'
 
-const props = defineProps<TUser>()
+const props = defineProps<Omit<TUserFull, 'comment'>>()
+const route = useRoute()
+
+const isCurrentActiveLink = computed(() => String(props.id) === String(route.params.id))
 </script>
 
 <template>
-  <NuxtLink :to="`/${props.id}`" class="w-full flex items-center justify-start gap-x-10 border-b-1 border-b-gray-500/50 px-5 py-3 duration-300 hover:(bg-gray-300)">
-    <span class="text-2xl text-black font-extrabold">{{ props.id }}</span>
+  <NuxtLink :to="`/${props.id}`" :class="{ 'bg-gray-400': isCurrentActiveLink }" class="w-full flex items-center justify-start gap-x-10 border-b-1 border-b-gray-500/50 px-5 py-3 duration-300 hover:(bg-gray-300)">
+    <span class="text-2xl text-black font-extrabold">{{ props.points }}</span>
     <p class="text-base text-black font-medium">
       {{ props.first_name }} {{ props.last_name }}
     </p>
@@ -14,4 +17,5 @@ const props = defineProps<TUser>()
   </NuxtLink>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
