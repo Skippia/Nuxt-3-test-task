f2f4440dfd857bb97278a6d7d3b85fe3ca716dde
Shelbymode <pocketbook.love24@gmail.com>
2024-03-29 11:21:42 +0300

    feat: add types & constants

--- types.ts
+++ types.ts
@@ -5,3 +5,16 @@ export interface TUser {
   last_name: string
   avatar: string
 }
+
+export interface TUserMiscellaneous {
+  id: number
+  comment: string
+  points: number
+}
+
+export interface TUserFull extends TUser, TUserMiscellaneous {}
+
+export interface TUpdatedUserCardEvent {
+  id: TUserFull['id']
+  points: TUserMiscellaneous['points']
+}
